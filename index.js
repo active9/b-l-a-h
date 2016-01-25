69ad4300148f740ebd6bd815d095e36cff290daa
Chris McCaulley <talkquazi@gmail.com>
2016-01-24 19:34:07 -0500

    npm test fix and cleanup

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var script = "";
 if (process.argv[0]=="node" | process.argv[0]=="nodejs") {
-	script = process.argv.slice(2,process.argv.length).join(" ");
+    script = process.argv.slice(2,process.argv.length).join(" ");
 } else {
-	script = process.argv.slice(1,process.argv.length).join(" ");
+    script = process.argv.slice(1,process.argv.length).join(" ");
 }
 
 if (typeof script === "undefined") {
-	console.log("BLAH > Missing Script Location or Input");
-	process.exit(1);
+    console.log("BLAH > Missing Script Location or Input");
+    process.exit(1);
 }
 
 require('./lib/blah.js')(script);
